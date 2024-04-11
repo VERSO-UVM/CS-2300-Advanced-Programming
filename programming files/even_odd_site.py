8040e846e47eb900ebe1e557838c30a3a149eb0e
0Davos <143449687+0Davos@users.noreply.github.com>
2024-04-11 10:41:32 -0400

    Update even_odd_site.py

--- programming files/even_odd_site.py
+++ programming files/even_odd_site.py
@@ -3,11 +3,11 @@ with open("numbers.html", "w") as f:
     f.write("<table>\n<tr><th>Even Numbers</th><th>Odd Numbers</th></tr>\n")
     for i in range(1, 50):
         if i % 2 == 0:
-            f.write("<tr><td>{}</td><td></td></tr>\n".format(i))
-        else:
-            f.write("<tr><td></td><td>{}</td></tr>\n".format(i))
+            g.write("<tr><td>{}</td><td></td></tr>\n".format(i))
+        if i % 3 == 0:
+            g.write("<tr><td></td><td>{}</td></tr>\n".format(i))
     f.write("</table>\n</body>\n</html>")
 
-with open("numbers.html", "r") as f:
+with open("numbers.html"") as f:
     print(f.read())
     
